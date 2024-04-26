e5eaa7a5408d69aa7cc68a80fcc6d0d0cc5c0412
marfin15 <donchenko.marf15@gmail.com>
2024-04-26 11:51:52 +0300

    the-second-commit

--- main_cats.py
+++ main_cats.py
@@ -16,12 +16,3 @@ def get_cats_info(path):
 cats_info = get_cats_info("cats.txt")
 if cats_info is not None:
     print(cats_info)
-
-
-[
-    {"id": "60b90c1c13067a15887e1ae1", "name": "Tayson", "age": "3"},
-    {"id": "60b90c2413067a15887e1ae2", "name": "Vika", "age": "1"},
-    {"id": "60b90c2e13067a15887e1ae3", "name": "Barsik", "age": "2"},
-    {"id": "60b90c3b13067a15887e1ae4", "name": "Simon", "age": "12"},
-    {"id": "60b90c4613067a15887e1ae5", "name": "Tessi", "age": "5"},
-]
